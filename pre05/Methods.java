e6a27fe95f18ecc61485c3d7909ffcb423018f29
richparker718 <96206153+richparker718@users.noreply.github.com>
2022-01-16 21:14:27 +0000

    completed isTriangle part of pre05

--- pre05/Methods.java
+++ pre05/Methods.java
@@ -6,10 +6,15 @@ public class Methods {
 public static boolean isDivisible(int m, int n){
   return (m % n == 0);
 }
+public static boolean isTriangle(int a, int b, int c){
+  return !(( a > b + c) || (b > a + c) || (c > a + b));
+  }
    
   public static void main(String[] args)
   {
-   System.out.println(isDivisible(24,2));
+   System.out.println(isTriangle(2,3,30));
+  
+   System.out.println(isDivisible(24,12));
   }
   }
   
