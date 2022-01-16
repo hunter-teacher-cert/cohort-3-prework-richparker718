b05de6b68b6506a82539a211983a62fed0e9c50c
richparker718 <96206153+richparker718@users.noreply.github.com>
2022-01-16 19:53:08 +0000

    completed basic isDivisible method; part 1 of pre05

---
+++ pre05/Methods.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Methods {
+
+public static boolean isDivisible(int m, int n){
+  return (m % n == 0);
+}
+   
+  public static void main(String[] args)
+  {
+   System.out.println(isDivisible(24,2));
+  }
+  }
+  
