066b4fd6988ef4a585590815f81b6638c9da461c
richparker718 <96206153+richparker718@users.noreply.github.com>
2022-01-16 22:14:41 +0000

    completed exercises 2,3,8 from Think Java; pre05 completed

--- pre05/Methods.java
+++ pre05/Methods.java
@@ -9,12 +9,22 @@ public static boolean isDivisible(int m, int n){
 public static boolean isTriangle(int a, int b, int c){
   return !(( a > b + c) || (b > a + c) || (c > a + b));
   }
+ public static int ackerman(int m, int n) {
+   if (m == 0) {
+     return (n + 1);
+   } else if (m > 0 && n == 0) {
+     return ackerman(m - 1, 1);
+   } else {
+     return ackerman(m - 1, ackerman(m, n - 1));
+   }
+ 
+ } 
    
   public static void main(String[] args)
   {
-   System.out.println(isTriangle(2,3,30));
-  
+   System.out.println(isTriangle(2,3,2));
    System.out.println(isDivisible(24,12));
+   System.out.println(ackerman(3,6));
   }
   }
   
