d210de54671bde4ae972b70b0f00b31a277aa3b0
richparker718 <96206153+richparker718@users.noreply.github.com>
2022-01-23 17:47:28 +0000

    changed name of file from Power.java to Loops.java; completed 2nd part of pre06 (factorial)

--- pre06/Loops.java
+++ pre06/Loops.java
@@ -2,24 +2,44 @@ import java.io.*;
 import java.util.*;
 
 
-public class Power{
+public class Loops{
 
   public static void power(int x, int n) {
     
     int base = x;
     
     if (n == 0) {
+
       System.out.println("1");
-    } else if (n == 1){
-      System.out.println(x);
+
     } else {
+
       for (int i = 1; i < n; i = i + 1){
        x = x * base;
+
       }
     System.out.println(x);
     }
     }
+
+    public static void factorial(int n) {
+    
+    
+    int result = 1;
+        
+      for (int i = n; i > 1; i = i - 1) {
+      
+       result = n * (i-1);
+       n = result;
+      }
+      System.out.println(result);
+      
+      } 
+    
+    
+
   public static void main(String[] args){
-      power(3,4);
+      power(3,3);
+      factorial(7);
 }
 }
