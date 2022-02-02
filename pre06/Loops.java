0262ce7ef8ee98fe2e93546217cf76a9903d97c0
richparker718 <96206153+richparker718@users.noreply.github.com>
2022-02-02 16:11:20 +0000

    completed 1st and 2nd part to exercise 5 pre06

--- pre06/Loops.java
+++ pre06/Loops.java
@@ -9,21 +9,17 @@ public class Loops{
     double base = x;
     
     if (n == 0) {
-
-      return 1;
+     return 1;
 
     } else {
-
       for (int i = 1; i < n; i = i + 1){
        x = x * base;
-
       }
       return x;
-    }
-    }
+    }}
+
 
     public static int factorial(int n) {
-    
     
     int result = 1;
         
@@ -36,6 +32,7 @@ public class Loops{
       return result;
       }
     
+
     public static double myexp(double x, int n){
       double sum = 1;
       int i = 1;
@@ -48,11 +45,37 @@ public class Loops{
       }
       return sum;
     }
+
+    public static double myexp2(double x, int n){
+      double sum = 1;
+      int i = 1;
+      double result = 1;
+
+      double Numer = x;
+      int Denom = i;
+      double nNumer = 1;
+      int nDenom = 1;
+
+      for (i = 1; i <=n; i++){
+       
+       Numer = nNumer;
+       Denom = nDenom;
+       nNumer = Numer * x;
+       nDenom = Denom * i;
+
+      result = nNumer/nDenom;
+      
+      sum = result + sum;
+        
+      }
+      return sum;
+    }
   
 
   public static void main(String[] args){
       System.out.println(power(1.5,3));
       System.out.println(factorial(7));
       System.out.println(myexp(2,6));
+      System.out.println(myexp2(2,6));
 }
 }
