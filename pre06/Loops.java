58dedf724823fbff8e7244e912b22c55bf547ee7
richparker718 <96206153+richparker718@users.noreply.github.com>
2022-02-03 20:46:34 +0000

    completed 3 and 4 for exercise 5 in pre06

--- pre06/Loops.java
+++ pre06/Loops.java
@@ -71,11 +71,17 @@ public class Loops{
       return sum;
     }
   
+  public static void check(double x){
+    System.out.println(x + "   " + myexp(x,4) + "   " + Math.exp(x));
+
+  }
+
 
   public static void main(String[] args){
       System.out.println(power(1.5,3));
       System.out.println(factorial(7));
       System.out.println(myexp(2,6));
       System.out.println(myexp2(2,6));
+      check(1);
 }
 }
