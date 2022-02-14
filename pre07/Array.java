5f59e559d1a1902bb4fff77342a87b85ccc42f23
richparker718 <96206153+richparker718@users.noreply.github.com>
2022-02-14 19:22:49 +0000

    added 2nd argument to 1st part of exercise 1 pre07

--- pre07/Array.java
+++ pre07/Array.java
@@ -3,27 +3,30 @@ import java.util.*;
 
 public class Array{
 
- 
-  public static double[] powArray(double[] a){
+  public static double[] powArray(double[] a, double b){
     
   
     for(int i= 0; i<a.length; i++){
-    
-    
+     
     System.out.print(Math.pow(a[i], 2.0) +" ");
     
 }
+    System.out.println();
+
+    for(int i= 0; i<a.length; i++){
+     
+    System.out.print(Math.pow(a[i], b) +" ");
     
+}
     return a;
     
 }
  public static void main(String[] args){
  
  double[] a= {3.0,4.0,5.0,6.0,7.0};
- powArray(a);
+ double b = 3.0;
+
+ powArray(a,b);
  
-}
-
-
-  
+} 
 }
