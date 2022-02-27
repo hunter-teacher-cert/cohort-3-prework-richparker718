fc0d3f21d8e723384a3c595b20c1942fa42be0ea
richparker718 <96206153+richparker718@users.noreply.github.com>
2022-02-27 17:30:32 +0000

    updated Craps.java

--- pre08/Craps.java
+++ pre08/Craps.java
@@ -5,22 +5,13 @@ import java.util.Scanner;
 public class Craps{
 
  public static void gameInstructions(){
-   newLine();
-System.out.println("GAME INSTRUCTIONS:");
-  
+newLine();
+System.out.println("GAME INSTRUCTIONS:");  
 System.out.print("A round is played by rolling the dice. ");
-   
-
-System.out.print("If a 2,3, or 12 is rolled that’s Craps and you lose. ");
-
-   
-System.out.print("If a 7 or 11 is rolled it's Natural and you win, ");
-   
-   
-System.out.print("otherwise the value rolled is the Point. ");
-
-   
-System.out.println("Continue to shoot(roll) until you either roll the Point again (win) or roll a 7 (lose).");
+System.out.print("If a 2,3, or 12 is rolled that’s Craps and you lose. ");   
+System.out.print("If a 7 or 11 is rolled it's Natural and you win, ");   
+System.out.print("otherwise the value rolled is the Point. ");  
+System.out.println("Continue to shoot until you either roll the Point again (win) or roll a 7 (lose).");
 newLine();
 newLine();
  }
@@ -28,17 +19,16 @@ newLine();
 
 
 public static void newLine(){
-    System.out.println();
+ System.out.println();
   }
 
 public static int roll(int a){
-
 Random random = new Random();
 int n= random.nextInt(a)+1;
  return n;
 }
   
-  public static void pauseToContinue(){
+public static void pauseToContinue(){
   Scanner in = new Scanner(System.in);
   in.nextLine();
   }
@@ -50,24 +40,21 @@ public static int shoot(int a, int b){
 }
 
  public static void round() {
-
    int i =1;
    int a = shoot(2,6);
    System.out.println("roll " + i +": " +a);
 
    if(!(a==2||a==3||a==12||a==7||a==11)){
-       System.out.println("Your Point is: " +a );
+     System.out.println("Your Point is: " +a );
    }
   
    if (a==2||a==3||a==12){
      System.out.println("CRAPS, YOU LOSE!");
      
    } else if(a==7||a==11){
-     
      System.out.println("NATURAL, YOU WIN!");
      
    }else{
-
      int point =a;
      int b=shoot(2,6);
      i++;
@@ -98,7 +85,7 @@ public static int shoot(int a, int b){
 public static void main(String[] args){
   gameInstructions();
   for (int i=1; i<= Integer.parseInt(args[0]); i++){
-    System.out.println("ROUND " +i); System.out.println("click return to roll dice");
+    System.out.println("ROUND " +i); System.out.println("press return to roll dice");
     round();
     newLine();
     newLine();
